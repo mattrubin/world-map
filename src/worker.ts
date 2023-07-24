dfb6bfe5b77aa0f3dcc35c7d45b6d3e7ca309a97
Matt Rubin <opensource@mattrubin.me>
2023-07-23 20:48:47 -0400

    Convert map coloring logic to TypeScript

--- src/worker.ts
+++ src/worker.ts
@@ -1,35 +1,49 @@
 import map from '../map.svg';
 
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
-}
+export interface Env {}
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const response = new Response(map);
+    const params = new URL(request.url).searchParams;
+
+    const color1 = params.get('color1') ?? '660099';
+    const color2 = params.get('color2') ?? '0000FF';
+    const color3 = params.get('color3') ?? '00FF00';
+    const color4 = params.get('color4') ?? 'FFFF00';
+    const color5 = params.get('color5') ?? 'FF0000';
+
+    const cutoff1 = Number(params.get('cutoff1') ?? '2');
+    const cutoff2 = Number(params.get('cutoff2') ?? '5');
+    const cutoff3 = Number(params.get('cutoff3') ?? '10');
+    const cutoff4 = Number(params.get('cutoff4') ?? '20');
+
+    const colors = new Map<string, string>();
+    for (const [rawKey, rawValue] of params) {
+      const key = rawKey.toLowerCase();
+      const value = Number(rawValue);
+      if (key.length == 2) {
+        if (value >= cutoff4) {
+          colors.set(key, color5);
+        } else if (value >= cutoff3) {
+          colors.set(key, color4);
+        } else if (value >= cutoff2) {
+          colors.set(key, color3);
+        } else if (value >= cutoff1) {
+          colors.set(key, color2);
+        } else if (value > 0) {
+          colors.set(key, color1);
+        }
+      }
+    }
+
+    let styleString = '.oceanxx { fill: #B5D6FE; }\n.landxx, .coastxx, .antxx { stroke: #000; stroke-width:0.3; }\n';
+    for (const [region, color] of colors) {
+      styleString += `.${region} { fill: #${color}; }\n`;
+    }
+
+    const mapWithStyle = map.replace('/** STYLE **/', styleString);
+
+    const response = new Response(mapWithStyle);
     response.headers.set('Content-type', 'image/svg+xml');
     return response;
   },
